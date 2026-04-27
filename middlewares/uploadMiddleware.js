a3f3de37ad2409704ba864fcb6e5322b4a2aa4d7
Rizvana12 <kmrizvana3@gmail.com>
2026-04-27 15:57:11 +0530

    fixed code

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,10 +1,11 @@
 const multer = require("multer");
+const path = require("path");
 
 //configure storage
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, path.join(__dirname, '../uploads'));
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
